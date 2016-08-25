5f5c3e1b028f2bd1d7b547a6355e3e0d277958c9
pran47 <pran47@gmail.com>
2016-08-25 13:13:20 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,329 @@
+<?php include('includes/header.php');?>
+
+<section id="slider"><!--slider-->
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12">
+					<div id="slider-carousel" class="carousel slide" data-ride="carousel">
+						<ol class="carousel-indicators">
+							<li data-target="#slider-carousel" data-slide-to="0" class="active"></li>
+							<li data-target="#slider-carousel" data-slide-to="1"></li>
+							<li data-target="#slider-carousel" data-slide-to="2"></li>
+						</ol>
+						
+						<div class="carousel-inner">
+							<div class="item active">
+								<div class="col-sm-6">
+									<h1><span>E</span>-SHOPPER</h1>
+									<h2>Free E-Commerce Template</h2>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+									<button type="button" class="btn btn-default get">Get it now</button>
+								</div>
+								<div class="col-sm-6">
+									<img src="images/home/girl1.jpg" class="girl img-responsive" alt="" />
+									<img src="images/home/pricing.png"  class="pricing" alt="" />
+								</div>
+							</div>
+							<div class="item">
+								<div class="col-sm-6">
+									<h1><span>E</span>-SHOPPER</h1>
+									<h2>100% Responsive Design</h2>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+									<button type="button" class="btn btn-default get">Get it now</button>
+								</div>
+								<div class="col-sm-6">
+									<img src="images/home/girl2.jpg" class="girl img-responsive" alt="" />
+									<img src="images/home/pricing.png"  class="pricing" alt="" />
+								</div>
+							</div>
+							
+							<div class="item">
+								<div class="col-sm-6">
+									<h1><span>E</span>-SHOPPER</h1>
+									<h2>Free Ecommerce Template</h2>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+									<button type="button" class="btn btn-default get">Get it now</button>
+								</div>
+								<div class="col-sm-6">
+									<img src="images/home/girl3.jpg" class="girl img-responsive" alt="" />
+									<img src="images/home/pricing.png" class="pricing" alt="" />
+								</div>
+							</div>
+							
+						</div>
+						
+						<a href="#slider-carousel" class="left control-carousel hidden-xs" data-slide="prev">
+							<i class="fa fa-angle-left"></i>
+						</a>
+						<a href="#slider-carousel" class="right control-carousel hidden-xs" data-slide="next">
+							<i class="fa fa-angle-right"></i>
+						</a>
+					</div>
+					
+				</div>
+			</div>
+		</div>
+	</section><!--/slider-->
+
+<?php include('includes/sidebar.php');?>
+
+    <div class="features_items"><!--features_items-->
+						<h2 class="title text-center">Features</h2>
+						<div class="col-sm-4">
+							<div class="product-image-wrapper">
+								<div class="single-products">
+										<div class="productinfo text-center">
+											<i class="zmdi zmdi-case thumb_i"></i>
+											<h2>Jobs</h2>
+											<p>Stay updated</p>
+											<a href="#" class="btn btn-default add-to-cart">View <i class="fa fa-chevron-right"></i></a>
+										</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-4">
+							<div class="product-image-wrapper">
+								<div class="single-products">
+									<div class="productinfo text-center">
+										<i class="zmdi zmdi-book-image thumb_i"></i>
+										<h2>Books & Hobbies</h2>
+										<p>Stay updated</p>
+										<a href="#" class="btn btn-default add-to-cart">View <i class="fa fa-chevron-right"></i></a>
+									</div>
+								</div>
+							</div>
+						</div>
+                        <div class="col-sm-4">
+							<div class="product-image-wrapper">
+								<div class="single-products">
+									<div class="productinfo text-center">
+										<i class="zmdi zmdi-home thumb_i"></i>
+										<h2>Homes</h2>
+										<p>Stay updated</p>
+										<a href="#" class="btn btn-default add-to-cart">View <i class="fa fa-chevron-right"></i></a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-4">
+							<div class="product-image-wrapper">
+								<div class="single-products">
+									<div class="productinfo text-center">
+										<i class="zmdi zmdi-car thumb_i"></i>
+										<h2>Cars</h2>
+										<p>Stay updated</p>
+										<a href="#" class="btn btn-default add-to-cart">View <i class="fa fa-chevron-right"></i></a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-4">
+							<div class="product-image-wrapper">
+								<div class="single-products">
+									<div class="productinfo text-center">
+										<i class="zmdi zmdi-bike thumb_i"></i>
+										<h2>Bikes</h2>
+										<p>Stay updated</p>
+										<a href="#" class="btn btn-default add-to-cart">View <i class="fa fa-chevron-right"></i></a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-4">
+							<div class="product-image-wrapper">
+								<div class="single-products">
+									<div class="productinfo text-center">
+										<i class="zmdi zmdi-smartphone-android thumb_i"></i>
+										<h2>Mobiles & Tablets</h2>
+										<p>Stay updated</p>
+										<a href="#" class="btn btn-default add-to-cart">View <i class="fa fa-chevron-right"></i></a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-4">
+							<div class="product-image-wrapper">
+								<div class="single-products">
+									<div class="productinfo text-center">
+										<i class="zmdi zmdi-tv thumb_i"></i>
+										<h2>Electronics & Appliances</h2>
+										<p>Stay updated</p>
+										<a href="#" class="btn btn-default add-to-cart">View <i class="fa fa-chevron-right"></i></a>
+									</div>
+								</div>
+							</div>
+						</div>
+                        <div class="col-sm-4">
+							<div class="product-image-wrapper">
+								<div class="single-products">
+									<div class="productinfo text-center">
+										<i class="zmdi zmdi-graduation-cap thumb_i"></i>
+										<h2>Education & Training</h2>
+										<p>Stay updated</p>
+										<a href="#" class="btn btn-default add-to-cart">View <i class="fa fa-chevron-right"></i></a>
+									</div>
+								</div>
+							</div>
+						</div>
+                        <div class="col-sm-4">
+							<div class="product-image-wrapper">
+								<div class="single-products">
+									<div class="productinfo text-center">
+										<i class="zmdi zmdi-watch thumb_i"></i>
+										<h2>Fashion</h2>
+										<p>Stay updated</p>
+										<a href="#" class="btn btn-default add-to-cart">View <i class="fa fa-chevron-right"></i></a>
+									</div>
+								</div>
+							</div>
+						</div>
+                        <div class="col-sm-4">
+							<div class="product-image-wrapper">
+								<div class="single-products">
+									<div class="productinfo text-center">
+										<i class="zmdi zmdi-seat thumb_i"></i>
+										<h2>Home & Office</h2>
+										<p>Stay updated</p>
+										<a href="#" class="btn btn-default add-to-cart">View <i class="fa fa-chevron-right"></i></a>
+									</div>
+								</div>
+							</div>
+						</div>
+                        <div class="col-sm-4">
+							<div class="product-image-wrapper">
+								<div class="single-products">
+									<div class="productinfo text-center">
+										<i class="zmdi zmdi-car-taxi thumb_i"></i>
+										<h2>Cabs</h2>
+										<p>Stay updated</p>
+										<a href="#" class="btn btn-default add-to-cart">View <i class="fa fa-chevron-right"></i></a>
+									</div>
+								</div>
+							</div>
+						</div>
+                        <div class="col-sm-4">
+							<div class="product-image-wrapper">
+								<div class="single-products">
+									<div class="productinfo text-center">
+										<i class="zmdi zmdi-calendar thumb_i"></i>
+										<h2>Events</h2>
+										<p>Stay updated</p>
+										<a href="#" class="btn btn-default add-to-cart">View <i class="fa fa-chevron-right"></i></a>
+									</div>
+								</div>
+							</div>
+						</div>
+                        <div class="col-sm-4">
+							<div class="product-image-wrapper">
+								<div class="single-products">
+									<div class="productinfo text-center">
+										<i class="zmdi zmdi-laptop-chromebook thumb_i"></i>
+										<h2>Web apps</h2>
+										<p>Stay updated</p>
+										<a href="#" class="btn btn-default add-to-cart">View <i class="fa fa-chevron-right"></i></a>
+									</div>
+								</div>
+							</div>
+						</div>
+                        <div class="col-sm-4">
+							<div class="product-image-wrapper">
+								<div class="single-products">
+									<div class="productinfo text-center">
+										<i class="zmdi zmdi-code-smartphone thumb_i"></i>
+										<h2>Mobile apps</h2>
+										<p>Stay updated</p>
+										<a href="#" class="btn btn-default add-to-cart">View <i class="fa fa-chevron-right"></i></a>
+									</div>
+								</div>
+							</div>
+						</div>
+                        <div class="col-sm-4">
+							<div class="product-image-wrapper">
+								<div class="single-products">
+									<div class="productinfo text-center">
+										<i class="zmdi zmdi-wrench thumb_i"></i>
+										<h2>Services</h2>
+										<p>Stay updated</p>
+										<a href="#" class="btn btn-default add-to-cart">View <i class="fa fa-chevron-right"></i></a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div><!--features_items-->
+
+    <div class="recommended_items"><!--recommended_items-->
+						<h2 class="title text-center">Trending items</h2>
+						
+						<div class="table-responsive cart_info">
+                            <table class="table table-condensed category_table">
+                                <tbody>
+                                    <tr>
+                                        <td class="cart_product">
+                                            <a href=""><img src="images/category.jpg" alt=""></a>
+                                        </td>
+                                        <td class="cart_description">
+                                            <h4><a href="">Honda City</a></h4>
+                                            <p><small class="small text-muted">Cars » Honda</span></small></p>
+                                            <p><strong>Renjith</strong></p>
+                                            <p class="text-muted">
+                                                <i class="fa fa-calendar"></i> Tue Aug 23 &nbsp;&nbsp;
+                                                <i class="fa fa-clock-o"></i> 11:40 am &nbsp;&nbsp;
+                                                <i class="fa fa-eye"></i> 210
+                                            </p>
+                                        </td>
+                                        <td class="cart_total">
+                                            <p class="cart_total_price"><i class="fa fa-inr"></i> 500000</p>
+                                        </td>
+                                    </tr>
+                                    
+                                    <tr>
+                                        <td class="cart_product">
+                                            <a href=""><img src="images/category.jpg" alt=""></a>
+                                        </td>
+                                        <td class="cart_description">
+                                            <h4><a href="">Honda City</a></h4>
+                                            <p><small class="small text-muted">Cars » Honda</span></small></p>
+                                            <p><strong>Renjith</strong></p>
+                                            <p class="text-muted">
+                                                <i class="fa fa-calendar"></i> Tue Aug 23 &nbsp;&nbsp;
+                                                <i class="fa fa-clock-o"></i> 11:40 am &nbsp;&nbsp;
+                                                <i class="fa fa-eye"></i> 210
+                                            </p>
+                                        </td>
+                                        <td class="cart_total">
+                                            <p class="cart_total_price"><i class="fa fa-inr"></i> 500000</p>
+                                        </td>
+                                    </tr>
+                                    
+                                    <tr>
+                                        <td class="cart_product">
+                                            <a href=""><img src="images/category.jpg" alt=""></a>
+                                        </td>
+                                        <td class="cart_description">
+                                            <h4><a href="">Honda City</a></h4>
+                                            <p><small class="small text-muted">Cars » Honda</span></small></p>
+                                            <p><strong>Renjith</strong></p>
+                                            <p class="text-muted">
+                                                <i class="fa fa-calendar"></i> Tue Aug 23 &nbsp;&nbsp;
+                                                <i class="fa fa-clock-o"></i> 11:40 am &nbsp;&nbsp;
+                                                <i class="fa fa-eye"></i> 210
+                                            </p>
+                                        </td>
+                                        <td class="cart_total">
+                                            <p class="cart_total_price"><i class="fa fa-inr"></i> 500000</p>
+                                        </td>
+                                    </tr>                                    
+                                </tbody>
+                            </table>
+                        </div>
+						
+						<ul class="pagination">
+							<li class="active"><a href="">1</a></li>
+							<li><a href="">2</a></li>
+							<li><a href="">3</a></li>
+							<li><a href="">&raquo;</a></li>
+						</ul>                        
+                        
+					</div><!--/recommended_items-->
+
+<?php include('includes/footer.php');?>
