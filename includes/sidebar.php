a0a46049a9e2bff032cfdf7699f99672f1b18489
pran47 <pran47@gmail.com>
2016-08-25 13:21:45 +0530

    Add files via upload

---
+++ includes/sidebar.php
@@ -0,0 +1,348 @@
+<section>
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-3">
+					<div class="left-sidebar">
+						<h2>All categories</h2>
+						<div class="panel-group category-products" id="accordian"><!--category-productsr-->
+							<div class="panel panel-default">
+								<div class="panel-heading">
+									<h4 class="panel-title">
+										<a data-toggle="collapse" data-parent="#accordian" href="#sportswear">
+											<span class="badge pull-right"><i class="fa fa-plus"></i></span>
+											Jobs
+										</a>
+									</h4>
+								</div>
+								<div id="sportswear" class="panel-collapse collapse">
+									<div class="panel-body">
+										<ul>
+											<li><a href="#">Designations </a></li>
+											<li><a href="#">Designations </a></li>
+											<li><a href="#">Designations </a></li>
+											<li><a href="#">Designations</a></li>
+											<li><a href="#">Designations </a></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+                            
+                            <div class="panel panel-default">
+								<div class="panel-heading">
+									<h4 class="panel-title">
+										<a data-toggle="collapse" data-parent="#accordian" href="#edu_train">
+											<span class="badge pull-right"><i class="fa fa-plus"></i></span>
+											Education & Training
+										</a>
+									</h4>
+								</div>
+								<div id="edu_train" class="panel-collapse collapse">
+									<div class="panel-body">
+										<ul>
+											<li><a href="#">Exam coching</a></li>
+											<li><a href="#">Workshops</a></li>
+											<li><a href="#">Career counselling</a></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+                            
+							<div class="panel panel-default">
+								<div class="panel-heading">
+									<h4 class="panel-title">
+										<a data-toggle="collapse" data-parent="#accordian" href="#mens">
+											<span class="badge pull-right"><i class="fa fa-plus"></i></span>
+											Books & Hobbies
+										</a>
+									</h4>
+								</div>
+								<div id="mens" class="panel-collapse collapse">
+									<div class="panel-body">
+										<ul>
+											<li><a href="#">Fendi</a></li>
+											<li><a href="#">Guess</a></li>
+											<li><a href="#">Valentino</a></li>
+											<li><a href="#">Dior</a></li>
+											<li><a href="#">Versace</a></li>
+											<li><a href="#">Armani</a></li>
+											<li><a href="#">Prada</a></li>
+											<li><a href="#">Dolce and Gabbana</a></li>
+											<li><a href="#">Chanel</a></li>
+											<li><a href="#">Gucci</a></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+							
+							<div class="panel panel-default">
+								<div class="panel-heading">
+									<h4 class="panel-title">
+										<a data-toggle="collapse" data-parent="#accordian" href="#womens">
+											<span class="badge pull-right"><i class="fa fa-plus"></i></span>
+											Homes
+										</a>
+									</h4>
+								</div>
+								<div id="womens" class="panel-collapse collapse">
+									<div class="panel-body">
+										<ul>
+											<li><a href="#">Appartment</a></li>
+											<li><a href="#">Flats</a></li>
+											<li><a href="#">Rooms</a></li>
+											<li><a href="#">Villas</a></li>
+											<li><a href="#">3bhk</a></li>
+                                            <li><a href="#">Room sharing</a></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+                            
+							<div class="panel panel-default">
+								<div class="panel-heading">
+									<h4 class="panel-title">
+										<a data-toggle="collapse" data-parent="#accordian" href="#cars">
+											<span class="badge pull-right"><i class="fa fa-plus"></i></span>
+											Cars
+										</a>
+									</h4>
+								</div>
+								<div id="cars" class="panel-collapse collapse">
+									<div class="panel-body">
+										<ul>
+											<li><a href="#">XUV</a></li>
+											<li><a href="#">Honda</a></li>
+											<li><a href="#">Maruti</a></li>
+											<li><a href="#">Mahindra</a></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+                            
+                            <div class="panel panel-default">
+								<div class="panel-heading">
+									<h4 class="panel-title">
+										<a data-toggle="collapse" data-parent="#accordian" href="#bikes">
+											<span class="badge pull-right"><i class="fa fa-plus"></i></span>
+											Bikes
+										</a>
+									</h4>
+								</div>
+								<div id="bikes" class="panel-collapse collapse">
+									<div class="panel-body">
+										<ul>
+											<li><a href="#">Bajaj</a></li>
+											<li><a href="#">Yamaha</a></li>
+											<li><a href="#">Honda</a></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+							
+                            <div class="panel panel-default">
+								<div class="panel-heading">
+									<h4 class="panel-title">
+										<a data-toggle="collapse" data-parent="#accordian" href="#mob_tabs">
+											<span class="badge pull-right"><i class="fa fa-plus"></i></span>
+											Mobiles & Tablets
+										</a>
+									</h4>
+								</div>
+								<div id="mob_tabs" class="panel-collapse collapse">
+									<div class="panel-body">
+										<ul>
+											<li><a href="#">Apple</a></li>
+											<li><a href="#">Sony</a></li>
+											<li><a href="#">Motorola</a></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+							
+							<div class="panel panel-default">
+								<div class="panel-heading">
+									<h4 class="panel-title">
+										<a data-toggle="collapse" data-parent="#accordian" href="#elec_appl">
+											<span class="badge pull-right"><i class="fa fa-plus"></i></span>
+											Electronics & Appliances
+										</a>
+									</h4>
+								</div>
+								<div id="elec_appl" class="panel-collapse collapse">
+									<div class="panel-body">
+										<ul>
+											<li><a href="#">TV</a></li>
+											<li><a href="#">Fridge</a></li>
+											<li><a href="#">Washing machine</a></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+							
+							<div class="panel panel-default">
+								<div class="panel-heading">
+									<h4 class="panel-title">
+										<a data-toggle="collapse" data-parent="#accordian" href="#fashion">
+											<span class="badge pull-right"><i class="fa fa-plus"></i></span>
+											Fashion
+										</a>
+									</h4>
+								</div>
+								<div id="fashion" class="panel-collapse collapse">
+									<div class="panel-body">
+										<ul>
+											<li><a href="#">Motorcycle racing suit</a></li>
+											<li><a href="#">Black and blue formal blazer</a></li>
+											<li><a href="#">Unused Tag Heuer frame</a></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+                            
+                            <div class="panel panel-default">
+								<div class="panel-heading">
+									<h4 class="panel-title">
+										<a data-toggle="collapse" data-parent="#accordian" href="#home_office">
+											<span class="badge pull-right"><i class="fa fa-plus"></i></span>
+											Home & Office
+										</a>
+									</h4>
+								</div>
+								<div id="home_office" class="panel-collapse collapse">
+									<div class="panel-body">
+										<ul>
+											<li><a href="#">Office chair</a></li>
+											<li><a href="#">Sofa</a></li>
+											<li><a href="#">Wardrobe</a></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+                            
+                            <div class="panel panel-default">
+								<div class="panel-heading">
+									<h4 class="panel-title">
+										<a data-toggle="collapse" data-parent="#accordian" href="#cabs">
+											<span class="badge pull-right"><i class="fa fa-plus"></i></span>
+											Cabs
+										</a>
+									</h4>
+								</div>
+								<div id="cabs" class="panel-collapse collapse">
+									<div class="panel-body">
+										<ul>
+											<li><a href="#">Car taxi</a></li>
+											<li><a href="#">Bus</a></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+                            
+                            <div class="panel panel-default">
+								<div class="panel-heading">
+									<h4 class="panel-title">
+										<a data-toggle="collapse" data-parent="#accordian" href="#events">
+											<span class="badge pull-right"><i class="fa fa-plus"></i></span>
+											Events
+										</a>
+									</h4>
+								</div>
+								<div id="events" class="panel-collapse collapse">
+									<div class="panel-body">
+										<ul>
+											<li><a href="#">Sports</a></li>
+											<li><a href="#">Arts</a></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+                            
+                            <div class="panel panel-default">
+								<div class="panel-heading">
+									<h4 class="panel-title">
+										<a data-toggle="collapse" data-parent="#accordian" href="#webapps">
+											<span class="badge pull-right"><i class="fa fa-plus"></i></span>
+											Web apps
+										</a>
+									</h4>
+								</div>
+								<div id="webapps" class="panel-collapse collapse">
+									<div class="panel-body">
+										<ul>
+											<li><a href="#">Office management</a></li>
+											<li><a href="#">School management</a></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+                            
+                            <div class="panel panel-default">
+								<div class="panel-heading">
+									<h4 class="panel-title">
+										<a data-toggle="collapse" data-parent="#accordian" href="#mobileapps">
+											<span class="badge pull-right"><i class="fa fa-plus"></i></span>
+											Mobile apps
+										</a>
+									</h4>
+								</div>
+								<div id="mobileapps" class="panel-collapse collapse">
+									<div class="panel-body">
+										<ul>
+											<li><a href="#">Android</a></li>
+											<li><a href="#">Iphone</a></li>
+                                            <li><a href="#">Windows</a></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+                            
+                            <div class="panel panel-default">
+								<div class="panel-heading">
+									<h4 class="panel-title">
+										<a data-toggle="collapse" data-parent="#accordian" href="#services">
+											<span class="badge pull-right"><i class="fa fa-plus"></i></span>
+											Services
+										</a>
+									</h4>
+								</div>
+								<div id="services" class="panel-collapse collapse">
+									<div class="panel-body">
+										<ul>
+											<li><a href="#">Company services</a></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+							
+						</div><!--/category-products-->
+					
+						<div class="brands_products"><!--brands_products-->
+							<h2>Brands</h2>
+							<div class="brands-name">
+								<ul class="nav nav-pills nav-stacked">
+									<li><a href="#"> <span class="pull-right">(50)</span>Acne</a></li>
+									<li><a href="#"> <span class="pull-right">(56)</span>Grüne Erde</a></li>
+									<li><a href="#"> <span class="pull-right">(27)</span>Albiro</a></li>
+									<li><a href="#"> <span class="pull-right">(32)</span>Ronhill</a></li>
+									<li><a href="#"> <span class="pull-right">(5)</span>Oddmolly</a></li>
+									<li><a href="#"> <span class="pull-right">(9)</span>Boudestijn</a></li>
+									<li><a href="#"> <span class="pull-right">(4)</span>Rösch creative culture</a></li>
+								</ul>
+							</div>
+						</div><!--/brands_products-->
+						
+						<div class="price-range"><!--price-range-->
+							<h2>Price Range</h2>
+							<div class="well text-center">
+								 <input type="text" class="span2" value="" data-slider-min="0" data-slider-max="600" data-slider-step="5" data-slider-value="[250,450]" id="sl2" ><br />
+								 <b class="pull-left">$ 0</b> <b class="pull-right">$ 600</b>
+							</div>
+						</div><!--/price-range-->
+						
+						<div class="shipping text-center"><!--shipping-->
+							<img src="images/home/shipping.jpg" alt="" />
+						</div><!--/shipping-->
+					
+					</div>
+				</div>
+				
+				<div class="col-sm-9 padding-right">
